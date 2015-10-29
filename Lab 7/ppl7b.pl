5d8962e5e28f255e78d2f2f1a4cfb4d0ec97afee
yashkrsingh <yashkr.singh@gmail.com>
2015-10-29 12:00:00 +0530

    First Commit

---
+++ Lab 7/ppl7b.pl
@@ -0,0 +1,15 @@
+#Name		: Yash Kumar SIngh
+#Roll Number	: 1310110373
+#Program Title	: Basic PERL Script L7
+
+print "Enter your name ";
+$name = <STDIN>;
+print "Enter your email ";
+$email = <STDIN>;
+print "Enter your course ";
+$course = <STDIN>;
+
+print "\n-----STUDENT INFO-----\n";
+print "Name   : $name";
+print "Email  : $email";
+print "Course : $course\n";
